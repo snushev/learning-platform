2c4d5bbe080d6dc9293f0e2b044634760f767b16
snushev <snushev@gmail.com>
2025-11-23 20:23:55 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.12.10-slim
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 8000
+
+CMD ["sh", "-c", "python wait-for-db.py && python manage.py migrate && python manage.py runserver 0.0.0.0:8000"]
---
+++ wait-for-db.py
@@ -0,0 +1,24 @@
+from decouple import config
+import time
+import socket
+
+
+
+def wait_for_db():
+    host = config('DB_HOST', default='db')
+    port = int(config('DB_PORT', default=5432))
+
+    while True:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.settimeout(5)
+                s.connect((host, port))
+                print(f"Database {host}:{port} is ready!")
+                return
+        except socket.error:
+            print(f"Waiting for database {host}:{port}...")
+            time.sleep(5)
+
+
+if __name__ == "__main__":
+    wait_for_db()
